ca5ddab1c8ae570d8ee36e56f97044083404cd91
csvirume <csvirume@gmail.com>
2015-03-29 20:29:04 +0530

    first change

---
+++ similarity.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 19:31:23 2015
+
+@author: vikram
+"""
+
+from math import sqrt
+
+#Returns distance based similarity score for two person
+
+def sim_distance(prefs,person1,person2){
+    # Get the list of shared items
+    #  there
+
+}
